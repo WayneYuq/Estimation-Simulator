2faf5b726eecde0ff3c90786c61e1a058a529302
Wayne Yu <3154816042@qq.com>
2020-12-19 15:56:25 +0800

    Update GPS and magnetometer fuse

--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -203,12 +203,20 @@ MatrixXf QuadEstimatorEKF::GetRbgPrime(float roll, float pitch, float yaw)
   //   that your calculations are reasonable
 
   ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
-  RbgPrime(0, 0) = -cos(pitch) * sin(yaw);
-  RbgPrime(0, 1) = -sin(roll) * sin(pitch) * sin(yaw) - cos(roll) * cos(yaw);
-  RbgPrime(0, 2) = -cos(roll)*sin(pitch)*sin(yaw) + sin(roll)*cos(yaw);
-  RbgPrime(1, 0) = cos(pitch)*cos(yaw);
-  RbgPrime(1, 1) = sin(roll)*sin(pitch)*cos(yaw) - cos(roll)*sin(yaw);
-  RbgPrime(1, 2) = cos(roll)*sin(pitch)*cos(yaw) + sin(roll)*sin(yaw);
+  
+  float sPhi = sin(roll);
+  float cPhi = cos(roll);
+  float sTheta = sin(pitch);
+  float cTheta = cos(pitch);
+  float sPsi = sin(yaw);
+  float cPsi = cos(yaw);
+
+  RbgPrime(0, 0) = -cTheta * sPsi;
+  RbgPrime(0, 1) = -sPhi * sTheta * sPsi - cPhi * cPsi;
+  RbgPrime(0, 2) = -cPhi*sTheta*sPsi + sPhi*cPsi;
+  RbgPrime(1, 0) = cTheta*cPsi;
+  RbgPrime(1, 1) = sPhi*sTheta*cPsi - cPhi*sPsi;
+  RbgPrime(1, 2) = cPhi*sTheta*cPsi + sPhi*sPsi;
   
 
   /////////////////////////////// END STUDENT CODE ////////////////////////////
@@ -289,7 +297,21 @@ void QuadEstimatorEKF::UpdateFromGPS(V3F pos, V3F vel)
   //  - The GPS measurement covariance is available in member variable R_GPS
   //  - this is a very simple update
   ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
-
+  
+  zFromX(0) = ekfState(0);
+  zFromX(1) = ekfState(1);
+  zFromX(2) = ekfState(2);
+  zFromX(3) = ekfState(3);
+  zFromX(4) = ekfState(4);
+  zFromX(5) = ekfState(5);
+  
+  hPrime(0, 0) = 1;
+  hPrime(1, 1) = 1;
+  hPrime(2, 2) = 1;
+  hPrime(3, 3) = 1;
+  hPrime(4, 4) = 1;
+  hPrime(5, 5) = 1;
+  
   /////////////////////////////// END STUDENT CODE ////////////////////////////
 
   Update(z, hPrime, R_GPS, zFromX);
@@ -311,7 +333,15 @@ void QuadEstimatorEKF::UpdateFromMag(float magYaw)
   //  - The magnetomer measurement covariance is available in member variable R_Mag
   ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
 
-
+  hPrime(0, QUAD_EKF_NUM_STATES-1) = 1;
+  zFromX(0) = ekfState(6);
+  
+  // Normalize the difference
+  float diff = z(0) - zFromX(0);
+  
+  if (diff > F_PI) zFromX(0) += 2.f * F_PI;
+  if (diff < -F_PI) zFromX(0) -= 2.f * F_PI;
+  
   /////////////////////////////// END STUDENT CODE ////////////////////////////
 
   Update(z, hPrime, R_Mag, zFromX);
